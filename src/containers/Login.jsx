e72a16112bcea30d702d4c71ae3df4e4ce30fb0b
Natali Rivero <riveronati@gmail.com>
2022-11-08 21:44:20 -0500

    Clase 11 Finalizada

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
-import '../styles/login.scss';
+import '../styles/Login.scss';
 
 const Login = () => {
   return (
     <div className="login">
       <div className="form-container">
-        <img src="./logos/logo_yard_sale.svg" alt="logo" className="logo" />
-        <h1 className="title">Create a new password</h1>
-        <p className="subtitle">Enter a new passwrd for yue account</p>
+        <img
+          src="../../public/logos/logo_yard_sale.svg"
+          alt="logo"
+          className="logo"
+        />
         <form action="/" className="form">
+          <label for="email" className="label">
+            Email address
+          </label>
+          <input
+            type="text"
+            id="email"
+            placeholder="platzi@example.cm"
+            className="input input-email"
+          />
           <label for="password" className="label">
             Password
           </label>
@@ -18,21 +29,14 @@ const Login = () => {
             placeholder="*********"
             className="input input-password"
           />
-          <label for="new-password" className="label">
-            Password
-          </label>
-          <input
-            type="password"
-            id="new-password"
-            placeholder="*********"
-            className="input input-password"
-          />
           <input
             type="submit"
-            value="Confirm"
+            value="Log in"
             className="primary-button login-button"
           />
+          <a href="/">Forgot my password</a>
         </form>
+        <button className="secondary-button signup-button">Sign up</button>
       </div>
     </div>
   );
