7f21f454608e05aac6bb58c36e2f41e7b8901877
Natali Rivero <riveronati@gmail.com>
2022-11-10 12:07:45 -0500

    Clase 20 Finalizada

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,7 @@ module.exports = {
       '@components': path.resolve(__dirname, 'src/components/'),
       '@containers': path.resolve(__dirname, 'src/containers/'),
       '@pages': path.resolve(__dirname, 'src/pages/'),
+      '@hooks': path.resolve(__dirname, 'src/hooks/'),
       '@styles': path.resolve(__dirname, 'src/styles/'),
       '@logos': path.resolve(__dirname, 'src/assets/logos/'),
       '@icons': path.resolve(__dirname, 'src/assets/icons/'),
