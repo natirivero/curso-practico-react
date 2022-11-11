4511b413175abb271c27e67a8602beefda9c86b5
Natali Rivero <riveronati@gmail.com>
2022-11-11 18:41:16 -0500

    Clase 22 Finalizada

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,37 +1,52 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import '../styles/Login.scss';
 
 import logo from '@logos/logo_yard_sale.svg';
 
 const Login = () => {
+  const form = useRef(null);
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+
+    const formData = new FormData(form.current);
+    const data = {
+      username: formData.get('email'),
+      password: formData.get('password'),
+    };
+    console.log(data);
+  };
+
   return (
-    <div className="login">
-      <div className="form-container">
+    <div className="login-form">
+      <div className="login-form-container">
         <img src={logo} alt="logo" className="logo" />
-        <form action="/" className="form">
-          <label for="email" className="label">
+        <form action="/" className="form" ref={form}>
+          <label htmlFor="email" className="label">
             Email address
           </label>
           <input
             type="text"
-            id="email"
+            name="email"
             placeholder="platzi@example.cm"
             className="input input-email"
           />
-          <label for="password" className="label">
+          <label htmlFor="password" className="label">
             Password
           </label>
           <input
             type="password"
-            id="password"
+            name="password"
             placeholder="*********"
             className="input input-password"
           />
-          <input
+          <button
             type="submit"
-            value="Log in"
             className="primary-button login-button"
-          />
+            onClick={handleSubmit}
+          >
+            Login
+          </button>
           <a href="/">Forgot my password</a>
         </form>
         <button className="secondary-button signup-button">Sign up</button>
