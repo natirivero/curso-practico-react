8d46b11f0abe3ffb202be06a1f58a8943885226e
Natali Rivero <riveronati@gmail.com>
2022-11-13 10:38:52 -0500

    Clase 24 Finalizada

--- src/containers/MyOrder.jsx
+++ src/containers/MyOrder.jsx
@@ -1,17 +1,23 @@
-import React from 'react';
-import OrderItem from '../components/OrderItem';
-import '../styles/MyOrder.scss';
+import React, { useContext } from 'react';
+import OrderItem from '@components/OrderItem';
+import AppContext from '../context/AppContext';
+import '@styles/MyOrder.scss';
 
-import flechita from '@icons/flechita.svg';
+import arrow from '@icons/flechita.svg';
 
 const MyOrder = () => {
+  const { state } = useContext(AppContext);
+
   return (
     <aside className="MyOrder">
       <div className="title-container">
-        <img src={flechita} alt="arrow" />
+        <img src={arrow} alt="arrow" />
         <p className="title">My order</p>
       </div>
       <div className="my-order-content">
+        {state.cart.map((product) => (
+          <OrderItem product={product} key={`orderItem-${product.id}`} />
+        ))}
         <OrderItem />
         <div className="order">
           <p>
