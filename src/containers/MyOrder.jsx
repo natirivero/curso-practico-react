1f3897c5960c20670648261c0924c9f695f4b250
Natali Rivero <riveronati@gmail.com>
2022-11-13 11:20:04 -0500

    Clase 25 Finalizada

--- src/containers/MyOrder.jsx
+++ src/containers/MyOrder.jsx
@@ -8,6 +8,13 @@ import arrow from '@icons/flechita.svg';
 const MyOrder = () => {
   const { state } = useContext(AppContext);
 
+  const sumTotal = () => {
+    const reducer = (accumulator, currentValue) =>
+      accumulator + currentValue.price;
+    const sum = state.cart.reduce(reducer, 0);
+    return sum;
+  };
+
   return (
     <aside className="MyOrder">
       <div className="title-container">
@@ -18,12 +25,11 @@ const MyOrder = () => {
         {state.cart.map((product) => (
           <OrderItem product={product} key={`orderItem-${product.id}`} />
         ))}
-        <OrderItem />
         <div className="order">
           <p>
             <span>Total</span>
           </p>
-          <p>$560.00</p>
+          <p>${sumTotal()}</p>
         </div>
         <button className="primary-button">Checkout</button>
       </div>
